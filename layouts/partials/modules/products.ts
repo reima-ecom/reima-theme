1cd7ddd6abc59e4fa3b0f70765253b23c2f4de85
Eric Selin <eric.selin@gmail.com>
2020-10-21 11:18:23 +0200

    feat: Change product list image when clicking color swatch
    The `products` module now supports changing the product image when clicking a color swatch. This is implemented via JS and by making the color dot a link. Hence, if JS is disabled or not supported, the user is still taken to the product page.
    
    PR: #39

---
+++ layouts/partials/modules/products.ts
@@ -0,0 +1,13 @@
+function colorDotClick(e: MouseEvent) {
+  const colorDotLink = (e.target as HTMLElement).closest('[data-image-srcset]');
+  if (colorDotLink) {
+    e.preventDefault();
+    const srcset = colorDotLink.getAttribute('data-image-srcset');
+    colorDotLink.closest('li').querySelector('img').srcset = srcset;
+  }
+}
+
+document.querySelectorAll<HTMLElement>('.product-list ul').forEach((element) => {
+  console.log('adding event listener');
+  element.addEventListener('click', colorDotClick);
+});
