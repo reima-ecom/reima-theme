c836d177c1abdcf30718ca6f47c3b55f1167619c
Eric Selin <eric.selin@gmail.com>
2021-02-15 16:07:18 +0000

    fix: Enable Klarna test for demo site

--- demo/worker/sw.ts
+++ demo/worker/sw.ts
@@ -2,12 +2,16 @@ import {
   fetchFromHost,
   getEventListener,
   notFound404WhenTrailingSlash,
+  rewriteForTesting,
 } from "https://raw.githubusercontent.com/reima-ecom/site-worker/v0.1.1/mod.ts";
 
 const eventListener = getEventListener({
   getAsset: notFound404WhenTrailingSlash(
     fetchFromHost("reima-demo.netlify.app"),
   ),
+  responseTransformers: [
+    rewriteForTesting(["exp-klarna-banner"]),
+  ],
 });
 
 addEventListener("fetch", eventListener);
