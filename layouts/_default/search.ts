833a65e45b6999c0787efeed3bd534e6445c3824
Eric Selin <eric.selin@gmail.com>
2021-11-09 10:15:00 +0100

    Remove top-level await from search page JS
    Top-level await is not supported by esbuild currently.

--- layouts/_default/search.ts
+++ layouts/_default/search.ts
@@ -1,10 +1,13 @@
+/// <reference lib="dom" />
+
 import RSearchResults from "../partials/elements/r-search-results.ts";
 
 window.customElements.define("r-search-results", RSearchResults);
 
-await window.customElements.whenDefined("r-search-results");
+window.customElements.whenDefined("r-search-results").then(() => {
+  const query = new URLSearchParams(location.search).get("q");
+  if (!query) throw new Error("Need query parameter `q` for search");
 
-const query = new URLSearchParams(location.search).get("q");
-if (!query) throw new Error("Need query parameter `q` for search");
-
-document.querySelector<RSearchResults>("main r-search-results")?.searchAndRender(query);
+  document.querySelector<RSearchResults>("main r-search-results")
+    ?.searchAndRender(query);
+});
